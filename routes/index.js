a6d999128a5a3be0c2cee8bb8e4c0e12d840a9fc
Elyes Barnat <elyesbarnat42@gmail.com>
2018-01-31 23:43:57 +0100

    yey

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+//  res.send('Hey! It works!');
+//  const amir = { name: "amir", age: 12};
+//  res.json(amir);
+	res.render('hello')
 });
+
+// router.get('/reverse/:name', (req, res) => {
+//   const reverse = [...req.params.name].reverse().join('')
+//   res.send(reverse);
+// });
 
 module.exports = router;
