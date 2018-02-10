cb67f06cf9aac64ee97d9b4f2c1fc04c14a0472d
Elyes Barnat <elyesbarnat42@gmail.com>
2018-02-10 15:43:24 +0100

    saving address, longitude & latitude for each store

--- models/Store.js
+++ models/Store.js
@@ -14,7 +14,25 @@ const storeSchema = new mongoose.Schema({
     type: String,
     trime: true
   },
-  tags: [String]
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates!'
+    }],
+    address: {
+      type: String,
+      required: 'You must supply an address!'
+    }
+  }
 })
 // I need 'this' that's why I'm not using an arrow function
 // 'this' will be equals to the Store that we are trying to save
