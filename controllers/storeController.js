f13ccc195af0d5a4c4d463de3ad9b08964a30d8a
Elyes Barnat <elyesbarnat42@gmail.com>
2018-02-10 15:04:25 +0100

    creating an editing flow for stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -15,12 +15,33 @@ exports.addStore = (req, res) => res.render('editStore', { title: 'Add Store' })
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save()
-  req.flash('success', `Successfully created ${store.name}. Care to leave a review ?`)
+  req.flash('success', `Successfully created <strong>${store.name}</strong>. Care to leave a review ?`)
   res.redirect(`/store/${store.slug}`)
 }
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find()
-  console.log(stores)
   res.render('stores', { title: 'Stores', stores })
+}
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id })
+  // 2. confirm they are the owner of the store
+  // TODO
+  // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+  // find an update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true, // forced our models to run its required validators
+  }).exec()
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>.
+    <a href="/stores/${store.slug}">View store →</a>`)
+  res.redirect(`/stores/${store._id}/edit`)
+
+  //Redirect them to the store and tell them it worked
 }
