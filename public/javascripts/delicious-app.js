9f98e2ef81c8ce79a6436b835bbf1c4914da108c
Elyes Barnat <elyesbarnat42@gmail.com>
2018-02-26 00:34:23 +0100

    user can like a store and check his hearted stores

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,9 +1,10 @@
-import '../sass/style.scss';
+import '../sass/style.scss'
 
-import { $, $$ } from './modules/bling';
-import autocomplete from './modules/autocomplete';
-import typeAhead from './modules/typeAhead';
-import makeMap from './modules/map';
+import { $, $$ } from './modules/bling'
+import autocomplete from './modules/autocomplete'
+import typeAhead from './modules/typeAhead'
+import makeMap from './modules/map'
+import ajaxHeart from './modules/heart'
 
 const address = $('#address')
 const lng = $('#lng')
@@ -13,3 +14,5 @@ autocomplete(address, lat, lng)
 typeAhead($('.search'))
 
 makeMap($('#map'))
+
+$$('form.heart').on('submit', ajaxHeart)
