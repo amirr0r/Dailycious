e971c5e28aa75dd5b79110936b6271da79f5613b
Elyes Barnat <elyesbarnat42@gmail.com>
2018-02-25 16:52:07 +0100

    ajax search interface

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -2,8 +2,11 @@ import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
+import typeAhead from './modules/typeAhead';
 
 const address = $('#address')
 const lng = $('#lng')
 const lat = $('#lat')
+
 autocomplete(address, lat, lng)
+typeAhead($('.search'))
